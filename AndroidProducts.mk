0ce93e1b22bf1559833d3c0cbf2d48483b7c37a9
mdhaaib200520 <mdhasib200520@gmail.com>
2024-10-03 18:08:22 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,3 +18,7 @@
 
 PRODUCT_MAKEFILES := \
      $(LOCAL_DIR)/twrp_Symphony_Z50.mk
+COMMON_LUNCH_CHOICES := \
+    twrp_Symphony_Z50-user \
+    twrp_Symphony_Z50-userdebug \
+    twrp_Symphony_Z50-eng
