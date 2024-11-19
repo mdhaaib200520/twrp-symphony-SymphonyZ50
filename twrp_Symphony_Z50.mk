d7628aa427a3061aca15514ad30ba44edce2efb9
mdhaaib200520 <mdhasib200520@gmail.com>
2024-11-19 10:21:26 +0600

    twrp_Symphony_Z50.mk

--- twrp_Symphony_Z50.mk
+++ twrp_Symphony_Z50.mk
@@ -18,20 +18,20 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 
 # Inherit from Symphony_Z50 device
 $(call inherit-product, device/symphony/Symphony_Z50/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Symphony_Z50
-PRODUCT_NAME := omni_Symphony_Z50
+PRODUCT_NAME := twrp_Symphony_Z50
 PRODUCT_BRAND := Symphony
 PRODUCT_MODEL := Symphony Z50
 PRODUCT_MANUFACTURER := symphony
