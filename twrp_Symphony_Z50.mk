adfee51c20066a9339ced74c5ba4e6a429a543f9
mdhaaib200520 <mdhasib200520@gmail.com>
2024-12-10 13:09:02 +0600

    Update twrp_Symphony_Z50.mk

--- twrp_Symphony_Z50.mk
+++ twrp_Symphony_Z50.mk
@@ -1,38 +1,26 @@
 #
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-
-# Inherit from Symphony_Z50 device
-$(call inherit-product, device/symphony/Symphony_Z50/device.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-
-# Device identifier. This must come after all inclusions
+# Inherit from Symphony_Z50 device
+$(call inherit-product, device/symphony/Symphony_Z50/device.mk)
 PRODUCT_DEVICE := Symphony_Z50
 PRODUCT_NAME := twrp_Symphony_Z50
 PRODUCT_BRAND := Symphony
 PRODUCT_MODEL := Symphony Z50
 PRODUCT_MANUFACTURER := symphony
-PRODUCT_RELEASE_NAME := Symphony Symphony Z50
+
+PRODUCT_GMS_CLIENTID_BASE := alps-full_sm6757c_64-{country}
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_sm6757c_64-user 9 PPR1.180610.011 1595830521 release-keys"
+
+BUILD_FINGERPRINT := Symphony/Symphony_Z50/Symphony_Z50:9/PPR1.180610.011/1595830521:user/release-keys
